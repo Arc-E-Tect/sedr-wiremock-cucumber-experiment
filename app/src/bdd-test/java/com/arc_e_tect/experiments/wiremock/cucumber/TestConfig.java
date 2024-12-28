e40060c5b8aa52c3febf09229acc91a6fe68309a
Arc-E-Tect <ieising@hotmail.com>
2024-12-28 21:33:16 +0100

    chore: remove irrelevant files

--- app/src/bdd-test/java/com/arc_e_tect/experiments/wiremock/cucumber/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.arc_e_tect.experiments.wiremock.cucumber;
-
-import com.github.tomakehurst.wiremock.WireMockServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TestConfig {
-}
